381fb518ddd01c8c4aa44bd81f16a45cd00248b3
Marco Echevestre <devminotto@gmail.com>
2021-07-19 14:44:10 +0200

    Create summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from './styles';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Income</p>
+          <img src={incomeImg} alt="Income" />
+        </header>
+        <strong>$1000</strong>
+      </div>
+      <div>
+        <header>
+          <p>Outcome</p>
+          <img src={outcomeImg} alt="Outcome" />
+        </header>
+        <strong>- $200</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>$800</strong>
+      </div>
+    </Container>
+  );
+}

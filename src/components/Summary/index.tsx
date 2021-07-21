2a4f12e738be3bb93a6ff4c3e7b042046448ff01
Marco Echevestre <devminotto@gmail.com>
2021-07-21 17:49:58 +0200

    Implement summary calculum

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,6 +11,25 @@ import { Container } from './styles';
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -18,21 +37,37 @@ export function Summary() {
           <p>Income</p>
           <img src={incomeImg} alt="Income" />
         </header>
-        <strong>$1000</strong>
+        <strong>
+          {new Intl.NumberFormat('pl', {
+            style: 'currency',
+            currency: 'PLN',
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Outcome</p>
           <img src={outcomeImg} alt="Outcome" />
         </header>
-        <strong>- $200</strong>
+        <strong>
+          -{' '}
+          {new Intl.NumberFormat('pl', {
+            style: 'currency',
+            currency: 'PLN',
+          }).format(summary.withdraw)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>$800</strong>
+        <strong>
+          {new Intl.NumberFormat('pl', {
+            style: 'currency',
+            currency: 'PLN',
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
